d93ba7b82e614a72a81aab83882c1b5a80451f78
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-05-02 16:11:25 +0200

    lc dummy

--- kernel/kmain.ml
+++ kernel/kmain.ml
@@ -5,18 +5,18 @@ external put_char : char -> unit = "put_char" [@@noalloc]
 
 
 let rec kmain () =
-  Mem.init ();
+  (* Mem.init (); *)
   clear_screen ();
   String.iter put_char "Hello";
-  Vid.putc 'V';
+  (* Vid.putc 'V'; *)
   (* Vid.puts "Hello"; *)
-  let a = Mem.get_vma 0x000b8000 in
+  (* let a = Mem.get_vma 0x000b8000 in
   Bytes.unsafe_set a 0 'c';
 
   let b = Mem.alloc 1 in
   let _ = Mem.setc b 'B' in 
   Mem.setc a @@ Mem.getc b;
-  String.iter Vid.putc "V";
+  String.iter Vid.putc "V"; *)
 ;;
 
 kmain ();;
